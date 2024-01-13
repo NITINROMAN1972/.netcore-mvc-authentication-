471417ad235f16f05d4e8e3a27b735f1c02e1029
NITINROMAN1972 <romannitin5@gmail.com>
2024-01-13 11:42:00 +0530

    netcore mvc authentication

---
+++ EPMS/Controllers/AccessController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;using System.Security.Claims;using Microsoft.AspNetCore.Authentication;using Microsoft.AspNetCore.Authentication.Cookies;using EPMS.Models;namespace EPMS.Controllers{    public class AccessController : Controller    {        public IActionResult Login()        {
+            // checking if user is still logged in
+            ClaimsPrincipal claimUser = HttpContext.User;            if(claimUser.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Index", "Home");
+            }            return View();        }        [HttpPost]        public async Task<IActionResult> Login(VMLogin modelLogin)        {            if(modelLogin.Email == "milind@gmail.com" && modelLogin.Password == "101")
+            {
+                List<Claim> claims = new List<Claim>()
+                {
+                    // creating a new claim
+                    new Claim(ClaimTypes.NameIdentifier, modelLogin.Email),
+                    new Claim("OtherProperties", "Eg - Role, for role based authorization")
+
+                };
+
+                ClaimsIdentity claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+                AuthenticationProperties properties = new AuthenticationProperties()
+                {
+                    AllowRefresh = true,
+                    IsPersistent = modelLogin.KeepLoggedIn
+                };
+
+                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity), properties);
+
+                return RedirectToAction("Index", "Home");
+            }            ViewData["ValidateMessage"] = "User not found !";            return View();        }    }}
---
+++ EPMS/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using EPMS.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+
+namespace EPMS.Controllers
+{
+    // user will enter in Home controller only if he is logged in
+    [Authorize]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        // custom method
+        public async Task<IActionResult> LogOut()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Login", "Access");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
